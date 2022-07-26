c0527041e657641412eff488dc57732b84300237
LeitmotivHub <leandrolopez.creations@gmail.com>
2022-07-26 14:37:49 -0300

    Front End en proceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaYEducacionComponent } from './componentes/experiencia-y-educacion/experiencia-y-educacion.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { LogrosComponent } from './componentes/logros/logros.component';
+import { PorfolioService } from './servicios/porfolio.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaYEducacionComponent,
+    AptitudesComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PorfolioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/componentes/experiencia-y-educacion/experiencia-y-educacion.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/servicios/porfolio.service';
+
+@Component({
+  selector: 'app-experiencia-y-educacion',
+  templateUrl: './experiencia-y-educacion.component.html',
+  styleUrls: ['./experiencia-y-educacion.component.css']
+})
+export class ExperienciaYEducacionComponent implements OnInit {
+
+  listaDeEducacion: any;
+
+  constructor(private datosPorfolio:PorfolioService) { }
+
+  ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data => {this.listaDeEducacion=data.education;});
+  }
+
+}
